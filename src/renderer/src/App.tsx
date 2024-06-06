39dee37da1a1fbe6bddfa6ad825a4ea3ab84a0a9
sunniekapar <kaparsunnie@gmail.com>
2024-06-05 22:32:01 -0400

    setup shadcn

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,3 +1,14 @@
+import { Button } from '@/components/ui/button'
+import { useState } from 'react'
+
 export default function App() {
-  return <div className="text-red-600">hello world</div>
+  const [count, setCount] = useState(0)
+
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-8">
+      <Button onClick={() => setCount((count) => count + 1)}>Click me</Button>
+      <p className="text-2xl font-semibold">{count}</p>
+    </div>
+  )
 }
